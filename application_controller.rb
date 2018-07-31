3302dfb9ce37c29213256408233d3685f0493ba2
fatimalaghari13 <fatimalaghari13@gmail.com>
2018-07-31 20:33:34 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,15 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
+    @a= 7
+     @b= 9
+     @hometown="nashville"
     erb :index
   end
+  
+  # get '/' do 
+  # # @hometown="nashville"
+  # #   erb:index
+  # # end
 
 end
